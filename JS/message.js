6b3843c81837cdfb15e3c5428f7b93f65d4d5557
Viktorya Hakobyan <hakobyanviktorya8@gmail.com>
2020-04-13 17:04:59 +0400

    Arr DB user

---
+++ JS/message.js
@@ -0,0 +1,78 @@
+window.onload= function () {
+    addUserTable()
+};
+
+function addUserTable(){
+  for (let i = 0; i < users.length; i++){
+      tbody.innerHTML+=`
+        <tr>
+           <td id="userID" class="border py-2 px-2">${users[i].id}</td>
+           <td id="userName" class="border py-2 px-3">${users[i].name}</td>
+           <td id="userEmail" class="border py-2 px-3">${users[i].email}</td>
+           <td id="userAddress" class="border py-2 px-3">${users[i].address.street +" "+ users[0].address.suite}</td>
+           <td id="userPhone" class="border py-2 px-3">${users[i].phone}</td>
+           <td id="userWebsite" class="border py-2 px-3">${users[i].website}</td>
+        </tr>
+      `;
+  }
+}
+
+searchBtn.onclick = function(){
+  let isFound = false;
+  for (let i=0;i<users.length;i++){
+      if (searchInput.value == users[i].id){   //typeof number & string => ==
+          tbody.innerHTML = "";
+          tbody.innerHTML+=`
+            <tr>
+               <td id="userID" class="border py-2 px-2">${users[i].id}</td>
+               <td id="userName" class="border py-2 px-3">${users[i].name}</td>
+               <td id="userEmail" class="border py-2 px-3">${users[i].email}</td>
+               <td id="userAddress" class="border py-2 px-3">${users[i].address.street +" "+ users[0].address.suite}</td>
+               <td id="userPhone" class="border py-2 px-3">${users[i].phone}</td>
+               <td id="userWebsite" class="border py-2 px-3">${users[i].website}</td>
+            </tr>
+          `;
+          isFound = true;
+      }
+  }
+  if (!isFound){
+      alert("No user found!")
+  }
+};
+
+
+
+
+
+// const inputs = document.querySelectorAll("userList");
+// let table= document.getElementById("table");
+
+
+/* /!*
+  users.forEach((user) => {
+    
+    const {id,name:Name,username,email,address,phone,website,company} = user;
+
+  console.log(id,Name,username,email,address,phone,website,company)
+    
+  });
+  *!/
+
+/!*
+  users.map((user) => {
+    
+    const {id,name:Name,username,email,address,phone,website,company} = user;
+
+  console.log(id,Name,username,email,address,phone,website,company)
+    
+  });
+*!/
+users.map((user) => {
+    
+    const {...rest} = user;
+
+  console.log(rest)
+    
+  });
+ */
+ 
